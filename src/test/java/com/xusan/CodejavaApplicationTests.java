f5c3f93cb349f3566339b823e99328e4737aeb64
xusan <xusan19971109@126.com>
2020-01-03 22:07:33 +0800

    begin start

---
+++ src/test/java/com/xusan/CodejavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xusan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodejavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
